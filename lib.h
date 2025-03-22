89834d55fc6f9111c9a99972a456c86775a857b0
Senor-Piet <143126385+Senor-Piet@users.noreply.github.com>
2025-03-21 21:50:53 -0500

    Create lib.h

---
+++ lib.h
@@ -0,0 +1,16 @@
+void forward(int ltps, int rtps, int distance);
+void backward(int ltps,int rpts, int distance);
+void pright(int distance);
+void pleft(int distance);
+void right(int distance);
+void left(int distance);
+void pause(int time);
+void slowservo(int startposition, int servonumber, int counter, int counterspeed);
+void servototop();
+void ssp(int time, int port, int position);
+void forsquare();
+void backsquare();
+void follow(int distance);
+void bfollow(int distance);
+void wfl(int port);
+void sdi(int time);
